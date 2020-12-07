f01e8bdcbdef32bb734e4171f3fddd3608815f05
Tham Nguyen <57996008+thamnt2611@users.noreply.github.com>
2020-12-07 12:09:51 +0700

    view thong_ke
    co 2 file, moi cai la 1 JPanel

---
+++ ThongKeHoPane.java
@@ -0,0 +1,23 @@
+package views.PhatQuaFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ThongKeHoPane {
+    private JPanel rootPane;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton locButton;
+
+    public ThongKeHoPane() {
+        locButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
